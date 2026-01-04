ad570fbd0abe864f204f931fc57c945e3d4badfb
Saoussen-CH <saoussen.chaabnia.pro@gmail.com>
2026-01-04 14:59:18 +0100

    Rebuild with correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Saoussen-CH.github.io/', // Change this to '/' if using custom domain
+  base: '/',
 })
